f29021bb5eb46cb0dc3d049d470f651491d7b0da
Avius <avius.ravis@gmail.com>
2018-09-08 16:14:12 +0200

    hotfix
    - fixed issue when placing labor area with Alien Races mod
    - updated graphics of placing labor area so the rectangles are actually highlighted

---
+++ Source/Prefs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace BadPeople
+{
+    public enum ToggleMode
+    {
+        Disabled,
+        Enabled,
+        Auto,
+    }
+
+    public static class MyPrefs
+    {
+        private static ToggleMode _showDebugInfo;
+        public static ToggleMode ShowDebugInfo { get => _showDebugInfo; set { _showDebugInfo = value; } }
+
+        public static void ExposeData()
+        {
+            Scribe_Values.Look<ToggleMode>(ref _showDebugInfo, "show_debug_info", ToggleMode.Auto, true);
+        }
+    }
+}
