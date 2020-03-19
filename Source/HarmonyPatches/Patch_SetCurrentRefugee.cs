6ca9f94a472be29dc1f420a0be4657df6a8ae862
Hazzer <warlocksforge@gmail.com>
2020-03-19 20:06:49 +0100

    Handle raid refugee which was moved to quest

---
+++ Source/HarmonyPatches/Patch_SetCurrentRefugee.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.QuestGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using Verse;
+
+namespace BadPeople.HarmonyPatches
+{
+    [HarmonyPatch]
+    class Patch_SetCurrentRefugee
+    {
+        static MethodBase TargetMethod()
+        {
+            return typeof(Slate).GetMethod("Set").MakeGenericMethod(typeof(Pawn));
+        }
+        // Quest xml file: Scripts_Utility_RewardsCore.xml, Scripts_JoinerThreatCore.xml
+        static void PostFix(string name, Pawn var, bool isAbsoluteName = false)
+        {
+            if(name == "joiner")
+            {
+                BadPeopleUtility.CurrentRefugee = var;
+            }
+        }        
+    }
+}
