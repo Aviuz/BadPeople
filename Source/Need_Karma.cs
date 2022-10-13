d458fe8b8d0ff51ebe4c9f69b0fcc13889fb8dc4
Hazzer <warlocksforge@gmail.com>
2022-10-13 21:42:38 +0200

    1.4 update

--- Source/Need_Karma.cs
+++ Source/Need_Karma.cs
@@ -102,7 +102,7 @@ namespace BadPeople
             return stringBuilder.ToString();
         }
 
-        public override void DrawOnGUI(Rect rect, int maxThresholdMarkers = 2147483647, float customMargin = -1f, bool drawArrows = true, bool doTooltip = true, Rect? rectForTooltip = null)
+        public override void DrawOnGUI(Rect rect, int maxThresholdMarkers = 2147483647, float customMargin = -1f, bool drawArrows = true, bool doTooltip = true, Rect? rectForTooltip = null, bool drawLabel = true)
         {
             if (threshPercents == null)
                 threshPercents = new List<float>();
